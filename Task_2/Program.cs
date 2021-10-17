ae7d8fd847a1bb59366677883632ca70f380b9b3
BiziBoy <79360682+BiziBoy@users.noreply.github.com>
2021-10-18 02:35:07 +0300

    Добавьте файлы проекта.

---
+++ Task_2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task_2
+{
+  interface IInheritance
+  {
+    public virtual string GetInfo()
+    {
+      return  "Parent.";
+    }
+  }
+
+  struct Base: IInheritance
+  {
+    public string GetInfo()
+    {
+      return "Base";
+    }
+  }
+
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      
+    }
+  }
+}
---
+++ Veem_Lab1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+//Учебный пример реализации immutable (неизменяемого) класса без дальнейшей реализации
+namespace Veem_Lab1
+{
+  sealed class MyImmutableClass
+  {
+    private string name { get; }
+
+    private readonly int id;
+
+    public MyImmutableClass(string _name, int _id)
+    {
+      name = _name;
+      id = _id;
+    }
+
+    public MyImmutableClass ChangeId(ref MyImmutableClass myImmutableClass)
+    {
+      string nameTemp = myImmutableClass.name;
+      Console.Write("Введите новое значение Id: ");
+      int idTemp = int.Parse(Console.ReadLine());
+      MyImmutableClass temp = new(nameTemp, idTemp);
+      myImmutableClass = null;
+      return temp;
+    }
+  }
+}
